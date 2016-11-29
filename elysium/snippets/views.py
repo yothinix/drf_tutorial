41cedc298f9d229968cef69f528e772670c9d618
yothinix <yothin@proteus-tech.com>
2016-11-29 14:45:41 +0700

    add instance view for snippet

--- elysium/snippets/views.py
+++ elysium/snippets/views.py
@@ -26,3 +26,26 @@ class JSONResponse(HttpResponse):
                 return JSONResponse(serializer.data, status=201)
             return JSONResponse(serializer.errors, status=400)
 
+    @csrf_exempt
+    def snippet_detail(request, pk):
+        try:
+            snippet = Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            return HttpResponse(status=404)
+
+        if request.method == 'GET':
+            serializer = SnippetSerializer(snippet)
+            return JSONResponse(serializer.data)
+
+        elif request.modethod == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = SnippetSerializer(snippet, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JSONResponse(serializer.data)
+            return JSONResponse(serializer.errors, status=400)
+
+        elif request.method == 'DELETE':
+            snippet.delete()
+            return HttpResponse(status=204)
+
